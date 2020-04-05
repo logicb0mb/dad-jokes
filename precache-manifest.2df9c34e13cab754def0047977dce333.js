bc5935ad04e3a31349a70c410cc80919149e7ee1
shreyasshukla16 <shreyas.shukla26@gmail.com>
2020-04-06 00:47:49 +0530

    Updates

---
+++ precache-manifest.2df9c34e13cab754def0047977dce333.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9ff71fe344275edc85e8c6b567697e3",
+    "url": "/dad-jokes/index.html"
+  },
+  {
+    "revision": "d3dde0a8e7ec9103239d",
+    "url": "/dad-jokes/static/css/main.a124a41b.chunk.css"
+  },
+  {
+    "revision": "4f28bb8bcbf4e6be9855",
+    "url": "/dad-jokes/static/js/2.cceda12f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dad-jokes/static/js/2.cceda12f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3dde0a8e7ec9103239d",
+    "url": "/dad-jokes/static/js/main.dd1facfe.chunk.js"
+  },
+  {
+    "revision": "dc1a26ecfc6d80b2a7d4",
+    "url": "/dad-jokes/static/js/runtime-main.ec1f3f4e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dad-jokes/static/media/logo.5d5d9eef.svg"
+  }
+]);
